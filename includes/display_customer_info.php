ee799367822881c76c83d908af681a1f0e793c48
Shah jalal <rjshahjalal132@gmail.com>
2024-01-31 20:18:49 +0600

    insert a custom information successfully

--- includes/display_customer_info.php
+++ includes/display_customer_info.php
@@ -71,6 +71,73 @@ function display_customer_information_callback() {
             echo 'Shipping Address 1 : ' . $shipping_address_1 . '<br>';
             echo 'Shipping Address 2 : ' . $shipping_address_2 . '<br>';
 
+            // make curl post request
+
+            $curl = curl_init();
+
+            // cURL URL with placeholders replaced by variables
+            $curl_url = 'https://propelr.transactiongateway.com/api/transact.php'
+                . '?customer_vault=add_customer'
+                . '&security_key=H24zBu3uC7rn3JR7uY86NqhQH6TZCzkc'
+                . '&ccnumber=4111111111111111'
+                . '&ccexp=10%2F25'
+                . '&checkname=testcustomer1'
+                . '&checkaba=490000018'
+                . '&checkaccount=24413815'
+                . '&account_holder_type=personal'
+                . '&account_type=savings'
+                . '&sec_code=PPD'
+                . '&currency=USD'
+                . '&payment=creditcard'
+                . '&orderid=2014'
+                . '&order_description=orderdescription'
+                . '&merchant_defined_field_=merchant_defined_field_1%3DValue'
+                . '&first_name=' . urlencode( $first_name )
+                . '&last_name=' . urlencode( $last_name )
+                . '&address1=' . urlencode( $billing_address_1 )
+                . '&address2=' . urlencode( $billing_address_2 )
+                . '&city=' . urlencode( $billing_city )
+                . '&state=' . urlencode( $billing_state )
+                . '&zip=' . urlencode( $billing_postcode )
+                . '&country=' . urlencode( $billing_country )
+                . '&phone=' . urlencode( $customer_phone )
+                . '&email=' . urlencode( $customer_email )
+                . '&company=' . urlencode( $billing_company )
+                . '&fax=1234'
+                . '&shipping_id=2015'
+                . '&shipping_firstname=' . urlencode( $shipping_first_name )
+                . '&shipping_lastname=' . urlencode( $shipping_last_name )
+                . '&shipping_company=' . urlencode( $shipping_company )
+                . '&shipping_address1=' . urlencode( $shipping_address_1 )
+                . '&shipping_address2=' . urlencode( $shipping_address_2 )
+                . '&shipping_city=' . urlencode( $shipping_city )
+                . '&shipping_state=' . urlencode( $shipping_state )
+                . '&shipping_zip=' . urlencode( $shipping_postcode )
+                . '&shipping_country=' . urlencode( $shipping_country )
+                . '&shipping_phone=' . urlencode( $shipping_phone )
+                . '&shipping_fax=Shipping%20fax%20number'
+                . '&shipping_email=Shipping%20email%20address'
+                . '&acu_enabled=true';
+
+            curl_setopt_array(
+                $curl,
+                array(
+                    CURLOPT_URL            => $curl_url,
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING       => '',
+                    CURLOPT_MAXREDIRS      => 10,
+                    CURLOPT_TIMEOUT        => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST  => 'POST',
+                )
+            );
+
+            $response = curl_exec( $curl );
+
+            curl_close( $curl );
+            echo $response;
+
         } else {
             echo 'Order not found.';
         }
